3a45aabd070523daaa957e139568ca92dd90870d
steelbrain <aneesiqbalbhatti@gmail.com>
2015-01-05 21:06:10 +0500

    Initial Rewrite Commit

---
+++ main.js
@@ -0,0 +1,87 @@
+(function(w){
+  var instances = [],$;
+  class dQuery{
+    elements:Array;
+    length:Number;
+    constructor(selector){
+      var elements;
+      if(selector instanceof HTMLDocument || selector instanceof HTMLElement){
+        elements = [selector];
+      } else if(typeof selector === 'string'){
+        elements = document.querySelectorAll(selector);
+      } else if(typeof selector === 'object'){
+        elements = [];
+        $.each(selector,function(entry){
+          if(entry instanceof HTMLDocument || entry instanceof HTMLElement)
+            elements.push(entry);
+        });
+      } else {
+        throw new Error("No Elements provided");
+      }
+      this.elements = elements;
+      this.length = elements.length;
+    }
+    each(callback:Function):dQuery{
+      $.each(this.elements,callback);
+      return this;
+    }
+    attr(name:String,value:String){
+      if(this.length === 0)return;
+      if(typeof value === 'undefined'){
+        return this.elements[0].getAttribute(name);
+      } else {
+        this.each(function(){
+          this.setAttribute(name,value);
+        });
+        return this;
+      }
+    }
+    removeAttr(name:String):dQuery{
+      if(this.length === 0)return this;
+      this.each(function(){
+        this.removeAttribute(name);
+      });
+      return this;
+    }
+  }
+  class D{
+    static constructor(args):dQuery{
+      // check if it already exists in the instances array
+      return new dQuery(args);
+    }
+    static rand():String{
+      return (Math.random() + 1).toString(36).substring(7)
+    }
+    static extend(b,c){
+      var a = {},key;
+      for(key in b){
+        if(b.hasOwnProperty(key))
+          a[key] = b[key];
+      }
+      for(key in c){
+        if(c.hasOwnProperty(key) && a.hasOwnProperty(key))
+          a[key] = c[key];
+      }
+      return a;
+    }
+    static each(object,callback:Function):void{
+      var i;
+      if(object instanceof Array || object instanceof NodeList){
+        [].forEach.call(object,function(element,index,array){
+          callback.call(element,element,index,array);
+        });
+      } else if(typeof object.length !== 'undefined'){
+        for(i=0;i<object.length;++i){
+          callback.call(object[i],object[i],i,object);
+        }
+      } else {
+        for(i in object){
+          if(object.hasOwnProperty(i)){
+            callback.call(object[i],object[i],i,object);
+          }
+        }
+      }
+    }
+  }
+  w.$ = $ = D;
+})(window);
