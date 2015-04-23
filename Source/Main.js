3e397bf067f2f487ba874fe43e18f770c47124fc
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-23 01:28:23 -0700

    :new: Initial Rewrite Commit

---
+++ Source/Main.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let ArrayProto = Array.prototype;
+let Regex = {
+  ID: /^#\w+$/,
+  Class: /^.\w+$/,
+  TagName: /^\w+$/
+};
+class dQuery{
+  constructor(Elements){
+    this.Elements = Elements ? (Elements instanceof NodeList || Elements instanceof HTMLElement ? Elements : []) : [];
+  }
+}
+function $dQuery(Selector){
+  if(Regex.ID.test(Selector)){
+    return new dQuery(document.getElementById(Selector.substr(1)));
+  } else if(Regex.Class.test(Selector)){
+    return new dQuery(document.getElementsByClassName(Selector.substr(1)));
+  } else if(Regex.TagName.test(Selector)){
+    return new dQuery(document.getElementsByTagName(Selector));
+  } else {
+    return new dQuery(document.querySelectorAll(Selector));
+  }
+}
+if(typeof module !== 'undefined'){
+  module.exports = $dQuery;
+} else if(typeof exports !== 'undefined'){
+  exports.$ = $dQuery;
+} else if(typeof window !== 'undefined'){
+  window.$ = $dQuery;
+}
