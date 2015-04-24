f6e043de7ea4f1f5817fc2c3d76824da1bbc08cd
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-23 22:02:10 -0700

    :new: Implement $dQuery.extend

--- Source/Main.js
+++ Source/Main.js
@@ -572,6 +572,27 @@ $dQuery.Elements = function(Elements){
   }
 };
 
+$dQuery.extend = function(out){
+  out = out || {};
+
+  for (var i = 1; i < arguments.length; i++) {
+    var obj = arguments[i];
+
+    if (!obj) continue;
+
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (typeof obj[key] === 'object')
+          $dQuery.extend(out[key], obj[key]);
+        else
+          out[key] = obj[key];
+      }
+    }
+  }
+
+  return out;
+};
+
 $dQuery.noConflict = function(){
   window.$ = Old$;
 };
