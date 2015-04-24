407ae610bad40651d1a5018c4164f87dfb119a30
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-23 22:17:35 -0700

    :white_check_mark: Add Tests for Constructor

--- Specs/Main.js
+++ Specs/Main.js
@@ -1,5 +1,28 @@
+"use strict";
+// Specifications
 describe("dQuery", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+
+  describe("constructor", function(){
+
+    it("accepts css selectors", function(){
+      expect( $("div[id=Demo]").length ).toBe(1);
+    });
+    it("accepts id selectors", function(){
+      expect( $("#Demo").length ).toBe(1);
+    });
+    it("accepts class selectors", function(){
+      expect( $(".Demo").length ).toBe(1);
+    });
+    it("accepts tag name selectors", function(){
+      expect( $("custom-element").length ).toBe(1);
+    });
+    it("creates DOM from string", function(){
+      expect( $("<p>Hey</p><p>Yo</p>").length ).toBe(2);
+    });
+    it("accepts DOMElements", function(){
+      expect( $(document.getElementById('Demo')).length ).toBe(1);
+    });
+
   });
+
 });
